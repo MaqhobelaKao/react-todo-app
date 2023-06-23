61a3db27feff43c3b927b939f20a095dd0126a80
MaqhobelaKao <kaomaqhobela@gmail.com>
2023-06-23 13:08:19 +0200

    Todo project init commit with three 3 todo hard coded

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'Buy coffee', key: 1},
+    {text: 'Create an app', key: 2},
+    {text: 'Play on the switch', key: 3}
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/**Header */}
+      <Header/>
+      <View style={styles.content}>
+      {/** todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data = {todos}
+            renderItem ={({item})=>(
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +33,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+  },
+
+  list: {
+    marginTop: 20
+  }
+
+
 });
